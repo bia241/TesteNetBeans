997fbdcf874c1ec8650ae7fd0f9ec0fe0f6e891c
bia241 <blira6218@gmail.com>
2024-08-06 13:57:34 -0300

    Add files via upload

--- ApplicationRefrigerante/src/applicationrefrigerante/ApplicationRefrigerante.java
+++ ApplicationRefrigerante/src/applicationrefrigerante/ApplicationRefrigerante.java
@@ -12,7 +12,7 @@ public class ApplicationRefrigerante {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         
-        System.out.println("Olá, seja bem-vindo á página promocional do Refril!");
+        System.out.println("Olá, seja bem-vinde á página promocional do Refril!");
         
         System.out.println("Digite a quantidade desejada de refrigerantes:");
         int quant = ler.nextInt();
@@ -21,7 +21,7 @@ public class ApplicationRefrigerante {
         
         valor = 4.99;
         
-        if(quant >= 6){
+        if(quant >= 8){
             valor = 4.19;
         }
         
